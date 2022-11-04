bcb81b7bb38f1d468f5af54a978b104ff7e9cbd7
Ryan Carville <ryancarville@gmail.com>
2022-11-04 19:57:04 +0100

    add tech to project stack

--- src/staticData/projects.ts
+++ src/staticData/projects.ts
@@ -15,7 +15,7 @@ export const allProjects: Project[] = [
   {
     title: 'AIDONIC',
     imgAlt: 'AIDONIC',
-    contentText: 'Humanitarian Aid Blockchain base platform',
+    contentText: 'Blockchain based humanitarian aid platform',
     imgUrl: 'images/aidonic.png',
     urlPath: 'https://my.aidonic.io',
     techStack: [
@@ -24,6 +24,7 @@ export const allProjects: Project[] = [
       { name: 'Next.js', logoPath: 'images/icons/nextjs.png' },
       { name: 'React.js', logoPath: 'images/icons/react.png' },
       { name: 'Node.js', logoPath: 'images/icons/nodejs.png' },
+      { name: 'Stellar', logoPath: 'images/icons/stellar.png' },
       { name: 'Serverless', logoPath: 'images/icons/serverless.png' },
       { name: 'Lambdas', logoPath: 'images/icons/lambda.jpg' },
       { name: 'DynamoDB', logoPath: 'images/icons/dynamodb.png' }
@@ -32,7 +33,7 @@ export const allProjects: Project[] = [
   {
     title: 'Calabash Villa',
     imgAlt: 'Calabash Villa',
-    contentText: 'Luxury Villa Property Website',
+    contentText: 'Luxury villa property website',
     imgUrl: 'images/calabash.png',
     urlPath: 'https://calabashvillabequia.com',
     repoUrl: 'https://github.com/ryancarville/calabash-ssg',
@@ -48,7 +49,7 @@ export const allProjects: Project[] = [
   {
     title: 'AIDONIC - Marketing SPA',
     imgAlt: 'AIDONIC - Marketing SPA',
-    contentText: 'Single Page Application for marketing platform',
+    contentText: 'Single page application for marketing',
     imgUrl: 'images/aidonic-marketing.png',
     urlPath: 'https://aidonic.io',
     techStack: [
@@ -76,7 +77,7 @@ export const allProjects: Project[] = [
   {
     title: 'Node Navigator',
     imgAlt: 'Node Navigator',
-    contentText: 'Simple Connected Node Navigation Tool',
+    contentText: 'Simple connected node navigation tool',
     imgUrl: 'images/nodeNav.png',
     urlPath: 'https://saporo-node-navigator.vercel.app/',
     repoUrl: 'https://github.com/ryancarville/saporo-node-navigator',
