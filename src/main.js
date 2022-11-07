aa8e12cd31de972a7e9eed40723cd9de56467fae
Ryan Carville <ryancarville@gmail.com>
2022-11-07 18:51:33 +0100

    move redirect to main js file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import './app.css'
 import App from './App.svelte'
 
+// only server the home page
+// if path contains anything more than the domain redirect to the domain
+const { origin, href } = window.location;
+if (href.length > origin.length + 1) {
+  window.location.href = window.location.origin + '/';
+}
+
 const app = new App({
   target: document.getElementById('app')
 })
